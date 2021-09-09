f20cf869ed05bdbc3513ae152b4adc73ad74b9ff
Luke1760 <kjd1760@gmail.com>
2021-09-09 19:29:42 +0800

    fix:request errors

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   end
 
   def valid_token
-    @user = User.find_by_authentication_token(request.headers["AUTH-TOKEN"])
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
     if @user.present?
       return @user
     else
