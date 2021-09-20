f6dd9fc22e3d081a2f326db1e7987f95fd179ef3
Luke1760 <kjd1760@gmail.com>
2021-09-20 15:39:05 +0800

    create Book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_092255) do
+ActiveRecord::Schema.define(version: 2021_09_20_073833) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "image"
+    t.string "author"
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
