1f689664e1348d1dedc9a427e5c6efa33f0764d4
Luke1760 <kjd1760@gmail.com>
2021-09-20 15:41:01 +0800

    create Review model and set references to user and book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_073833) do
+ActiveRecord::Schema.define(version: 2021_09_20_074018) do
 
   create_table "books", force: :cascade do |t|
     t.string "image"
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2021_09_20_073833) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "content_rating"
+    t.integer "recommend_rating"
+    t.float "average_rating"
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +50,6 @@ ActiveRecord::Schema.define(version: 2021_09_20_073833) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
