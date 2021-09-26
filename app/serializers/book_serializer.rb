a7c58e2e60f0ce6a31cb912cbf5dc8d8e4614307
Luke1760 <kjd1760@gmail.com>
2021-09-26 18:04:46 +0800

    create action of ratings and add them as attributes

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,20 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :image
+  attributes :id, :title, :author, :image,
+             :content_rating_of_book,
+             :recommend_rating_of_book,
+             :average_rating_of_book
   has_many :reviews
+
+  # create action of ratings and add them as attributes
+  def content_rating_of_book
+    object.reviews.count == 0 ? 0 : object.reviews.average(:content_rating).round(1)
+  end
+
+  def recommend_rating_of_book
+    object.reviews.count == 0 ? 0 : object.reviews.average(:recommend_rating).round(1)
+  end
+
+  def average_rating_of_book
+    object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating).round(1)
+  end
 end
