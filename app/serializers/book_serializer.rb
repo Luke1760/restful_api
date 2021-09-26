4aafb4d2486241224a46fe2a9852e1a0b99cb3cd
Luke1760 <kjd1760@gmail.com>
2021-09-26 18:46:14 +0800

    improve performance by caching that number in one column

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,20 +1,25 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :image,
-             :content_rating_of_book,
-             :recommend_rating_of_book,
-             :average_rating_of_book
-  has_many :reviews
+            #  :content_rating_of_book,
+            #  :recommend_rating_of_book,
+            #  :average_rating_of_book
+             :total_reviews
+  # has_many :reviews
 
   # create action of ratings and add them as attributes
-  def content_rating_of_book
-    object.reviews.count == 0 ? 0 : object.reviews.average(:content_rating).round(1)
-  end
+  # def content_rating_of_book
+  #   object.reviews.count == 0 ? 0 : object.reviews.average(:content_rating).round(1)
+  # end
 
-  def recommend_rating_of_book
-    object.reviews.count == 0 ? 0 : object.reviews.average(:recommend_rating).round(1)
-  end
+  # def recommend_rating_of_book
+  #   object.reviews.count == 0 ? 0 : object.reviews.average(:recommend_rating).round(1)
+  # end
 
-  def average_rating_of_book
-    object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating).round(1)
+  # def average_rating_of_book
+  #   object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating).round(1)
+  # end
+
+  def total_reviews
+    object.reviews.count
   end
 end
