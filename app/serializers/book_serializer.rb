46c2e54645156b63b0a39b430bb3a5beb7181fa2
Luke1760 <kjd1760@gmail.com>
2021-09-26 22:28:43 +0800

    implement bullet gem to deal with N+1 query issue

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,10 +1,10 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :image,
+  attributes :id, :title, :author, :image
             #  :content_rating_of_book,
             #  :recommend_rating_of_book,
             #  :average_rating_of_book
-             :total_reviews
-  # has_many :reviews
+            #  :total_reviews
+  has_many :reviews
 
   # create action of ratings and add them as attributes
   # def content_rating_of_book
@@ -19,7 +19,7 @@ class BookSerializer < ActiveModel::Serializer
   #   object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating).round(1)
   # end
 
-  def total_reviews
-    object.reviews_count
-  end
+  # def total_reviews
+  #   object.reviews_count
+  # end
 end
